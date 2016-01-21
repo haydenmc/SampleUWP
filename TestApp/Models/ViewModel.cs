21b55d3eafc766d3cecfb6e56c058c356f6e1c72
Hayden <hayden@outlook.com>
2016-01-20 21:42:57 -0800

    Simple design and a viewmodel with some compiled data bindings.

---
+++ TestApp/Models/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Models
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string subredditName;
+        public string SubredditName
+        {
+            get
+            {
+                return subredditName;
+            }
+            set
+            {
+                if (value != subredditName)
+                {
+                    subredditName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public readonly ObservableCollection<Post> Posts = new ObservableCollection<Post>();
+
+        public ViewModel()
+        {
+            Posts.Add(new Post()
+            {
+                Title = "Test Post"
+            });
+        }
+    }
+}
